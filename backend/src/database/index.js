f2dc33ee7fe2a5663730e82e910a79f44b53f6a5
Felipe Alves <felipe.rod.alves@gmail.com>
2019-11-22 21:11:57 -0300

    feature: initial backend configuration
    - Add express, sucrase, nodemon
    - Add sequelize configuration
    - Add eslint / prettier / editorconfig
    - Add User model
    - Add User & Session Controller
    - Add authentication middleware

---
+++ backend/src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+import databaseConfig from '../config/database';
+
+import User from '../app/models/User';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection, Sequelize.DataTypes))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import authMiddleware from './app/middlewares/auth';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+export default routes;
