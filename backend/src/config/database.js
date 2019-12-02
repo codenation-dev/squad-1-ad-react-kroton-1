088b4a3c7f384832fa209b0aaa23c3608b76cb83
Felipe Alves <felipe.rod.alves@gmail.com>
2019-12-02 12:15:28 -0300

    Revert "feature: Docker (file & compose) + wait-for-it"
    This reverts commit 07a5490f2a11041cad79df2fcb463962c3fb602b.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,10 +3,10 @@ require('dotenv/config');
 module.exports = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
